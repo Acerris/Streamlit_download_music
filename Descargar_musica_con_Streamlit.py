8ca8c21165f508587262ea6a97275fa0e37aa23d
Acerris <fermindiaz01flores@gmail.com>
2023-03-12 13:43:23 -0500

    mejoras de reconocimiento

--- Descargar_musica_con_Streamlit.py
+++ Descargar_musica_con_Streamlit.py
@@ -2,31 +2,35 @@ import streamlit as st
 from pytube import YouTube
 from pytube.exceptions import VideoUnavailable
 import re
+from pydub import AudioSegment
+import speech_recognition as sr
 
 def is_valid_youtube_url(url):
     regex = r'(https?://)?(www\.)?youtube\.com/watch\?v=[\w-]{11}'
     return re.match(regex, url) is not None
 
+def is_valid_youtube_url_mobile(url):
+    regex = r'(https?://)?youtu\.be/[\w-]{11}'
+    return re.match(regex, url) is not None
+
 st.title(":blue[FD] Download :blue[FD]")
 # Ingresa la URL del video de YouTube que deseas descargar
-col1, col2= st.columns([3,1])
+col1, col2, col3= st.columns([3,2,1])
 try:
     with col1:
         url = st.text_input("Coloca el URL Aqui!")
         
         if is_valid_youtube_url(url):
-        # Crea un objeto de YouTube
+            yt = YouTube(url)
+        elif is_valid_youtube_url_mobile(url):
             yt = YouTube(url)
         else:
             st.write('Por favor, introduce una URL de video de YouTube válida')
     with col2:
         st.caption("Presiona para Empezar!")
         if st.button("Convertir Audio"):
-
         #titulo = yt.title
-# Filtra la transmisión de audio
             audio_stream = yt.streams.filter(only_audio=True).first()
-# Descarga el archivo de audio
         #audio_stream.download( output_path=".", filename=f"{titulo}.mp3") lo pondremos sin titulo por el momento
             audio_stream.download( output_path=".", filename=f"FDmusic.mp3")
             st.balloons()
@@ -39,6 +43,16 @@ try:
                     data=file,
                     file_name='FDmusic.mp3'
                     )
+            with col3:
+                if st.button("Convertir audio a texto"):
+                    audio = AudioSegment.from_mp3('FDmusic.mp3')
+                    audio.export('convert.wav', format='wav')
+                    r = sr.Recognizer()
+                    with sr.AudioFile('convert.wav') as source:
+                        audio = r.record(source)
+                    text = r.recognize_google(audio, language='es-ES')
+                    texto=st.text_area(text)
+                    print(text)
 except VideoUnavailable:
     print("DERECHOS DE AUTOR")
     st.write('NO SE PUEDE DESCARGAR POR DERECHOS DE AUTOR')
