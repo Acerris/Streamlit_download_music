934c62194e506555c31585930a7184e3f23eec97
Acerris <fermindiaz01flores@gmail.com>
2023-03-12 03:39:44 -0500

    Realizando mejoras con regex

--- Descargar_musica_con_Streamlit.py
+++ Descargar_musica_con_Streamlit.py
@@ -1,17 +1,25 @@
 import streamlit as st
 from pytube import YouTube
+import re
 
+def is_valid_youtube_url(url):
+    regex = r'(https?://)?(www\.)?youtube\.com/watch\?v=[\w-]{11}'
+    return re.match(regex, url) is not None
 
 st.title(":blue[FD] Download :blue[FD]")
 # Ingresa la URL del video de YouTube que deseas descargar
 col1, col2= st.columns([3,1])
 with col1:
     url = st.text_input("Coloca el URL Aqui!")
+    if is_valid_youtube_url(url):
+        # Crea un objeto de YouTube
+        yt = YouTube(url)
+    else:
+        st.write('Por favor, introduce una URL de video de YouTube válida')
 with col2:
     st.caption("Presiona para Empezar!")
     if st.button("Convertir Audio"):
-# Crea un objeto de YouTube
-        yt = YouTube(url)
+        
         #titulo = yt.title
 # Filtra la transmisión de audio
         audio_stream = yt.streams.filter(only_audio=True).first()
