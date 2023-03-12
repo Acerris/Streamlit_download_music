a4de512161865dde08cbff4d2ff0d0c320be8650
Acerris <fermindiaz01flores@gmail.com>
2023-03-12 04:02:39 -0500

    otros avances

--- Descargar_musica_con_Streamlit.py
+++ Descargar_musica_con_Streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from pytube import YouTube
+from pytube.exceptions import VideoUnavailable
 import re
 
 def is_valid_youtube_url(url):
@@ -9,30 +10,35 @@ def is_valid_youtube_url(url):
 st.title(":blue[FD] Download :blue[FD]")
 # Ingresa la URL del video de YouTube que deseas descargar
 col1, col2= st.columns([3,1])
-with col1:
-    url = st.text_input("Coloca el URL Aqui!")
-    if is_valid_youtube_url(url):
-        # Crea un objeto de YouTube
-        yt = YouTube(url)
-    else:
-        st.write('Por favor, introduce una URL de video de YouTube válida')
-with col2:
-    st.caption("Presiona para Empezar!")
-    if st.button("Convertir Audio"):
+try:
+    with col1:
+        url = st.text_input("Coloca el URL Aqui!")
         
+        if is_valid_youtube_url(url):
+        # Crea un objeto de YouTube
+            yt = YouTube(url)
+        else:
+            st.write('Por favor, introduce una URL de video de YouTube válida')
+    with col2:
+        st.caption("Presiona para Empezar!")
+        if st.button("Convertir Audio"):
+
         #titulo = yt.title
 # Filtra la transmisión de audio
-        audio_stream = yt.streams.filter(only_audio=True).first()
+            audio_stream = yt.streams.filter(only_audio=True).first()
 # Descarga el archivo de audio
         #audio_stream.download( output_path=".", filename=f"{titulo}.mp3") lo pondremos sin titulo por el momento
-        audio_stream.download( output_path=".", filename=f"FDmusic.mp3")
-        st.balloons()
-        with col1:
-            st.success("Audio bajado, ahora presiona en descargar y disfruta")
-        st.caption("Presiona para descargar!")
-        with open("FDmusic.mp3", "rb") as file:
-            st.download_button(
-                label="Descargar Musica",
-                data=file,
-                file_name='FDmusic.mp3'
-                )
+            audio_stream.download( output_path=".", filename=f"FDmusic.mp3")
+            st.balloons()
+            with col1:
+                st.success("Audio bajado, ahora presiona en descargar y disfruta")
+            st.caption("Presiona para descargar!")
+            with open("FDmusic.mp3", "rb") as file:
+                st.download_button(
+                    label="Descargar Musica",
+                    data=file,
+                    file_name='FDmusic.mp3'
+                    )
+except VideoUnavailable:
+    print("DERECHOS DE AUTOR")
+    st.write('NO SE PUEDE DESCARGAR POR DERECHOS DE AUTOR')
